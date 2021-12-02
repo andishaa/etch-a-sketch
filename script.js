a4e21e6e229f064532cfabe08528e6f33406519a
andisha <andon.stoichkov@yahoo.com>
2021-12-02 19:20:38 +0200

    Dynamically change grid layout

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+let gridNumber = 16;
+
 function createDivs() {
     const gridContainer = document.querySelector('.grid-container');
 
-    for (let i = 0; i < 32; i++) {
+    for (let i = 0; i < gridNumber * gridNumber; i++) {
         let newDiv = document.createElement('div');
         newDiv.className = 'grid-item';
         gridContainer.appendChild(newDiv);
+        
     }
+    gridContainer.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
 }
 createDivs();
